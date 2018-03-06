89063ad03e7a924ca0131ef6e323bb84f4426f63
dixonaws <dixonaws@amazon.com>
2018-03-06 00:44:04 +0000

    verify file in S3 by reading out its filesize

--- rds_pg_dumpall.sh
+++ rds_pg_dumpall.sh
@@ -8,6 +8,8 @@ for dbname in $databases; do
 	datetimestring=$(date +%m-%d-%Y_%T%z)
 
 	echo -n "Backing up $dbname... "
+
+	# you can't backup rdsadmin database on RDS
 	if [ $dbname = "rdsadmin" ]
 	then 
 		echo "skipping rdsadmin"
@@ -20,8 +22,14 @@ for dbname in $databases; do
 	# copy to S3 dbtools-backups bucket
 	aws s3 cp /home/ec2-user/backups/$targetfilename s3://dbtools-backups
 
-	filesize=$(ls -lah /home/ec2-user/backups/$targetfilename |awk '{print $5}')
-	echo "done ($targetfilename, $filesize)"
+	# get the filesize on the local filesystem
+	# filesize=$(ls -lah /home/ec2-user/backups/$targetfilename |awk '{print $5}')
+
+	# get the filesize in S3
+	filesize=$(aws s3 ls s3://dbtools-backups/$targetfilename |awk '{print $3}')
+	filesizemb=$(expr $filesize / 1024 / 1024)
+	filesizekb=$(expr $filesize / 1024)
+	echo "done ($targetfilename, $filesizekb KiB)"
 done
 
 
